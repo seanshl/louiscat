f0e034e5304dfeac47751e690986b6f5710a0fad
shiyaoeating <liushiyao2014@gmail.com>
2017-07-19 10:43:31 -0500

    Second commit

--- src/main/java/org/shiyao/intr/HelloServlet.java
+++ src/main/java/org/shiyao/intr/HelloServlet.java
@@ -17,6 +17,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String currentTime = dateFormat.format(new Date());
+
         request.setAttribute("currentTime", currentTime);
         request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,
                 response);
