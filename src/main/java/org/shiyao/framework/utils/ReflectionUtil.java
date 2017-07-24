269f9e4d47cbbbc81fdc02e2e1e54c2a149c03e8
shiyaoeating <liushiyao2014@gmail.com>
2017-07-24 13:06:53 -0500

    Add Reflection Util

--- src/main/java/org/shiyao/framework/utils/ReflectionUtil.java
+++ src/main/java/org/shiyao/framework/utils/ReflectionUtil.java
@@ -3,6 +3,9 @@ package org.shiyao.framework.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public final class ReflectionUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
 
@@ -18,5 +21,26 @@ public final class ReflectionUtil {
         return instance;
     }
 
+    public static Object invokeMethod(Object obj, Method method, Object... args) {
+        Object result;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(obj, args);
+        } catch (Exception e) {
+            LOGGER.error("Failed invoking method", e.getMessage());
+            throw new RuntimeException(e);
+        }
 
+        return result;
+    }
+
+    public static void setField(Object obj, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+        } catch (Exception e){
+            LOGGER.error("Set field failure", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
