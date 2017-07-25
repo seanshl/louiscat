ad0c7280d0ae84ac24bf7a4be7944cafe928ac85
shiyaoeating <liushiyao2014@gmail.com>
2017-07-25 11:16:54 -0500

    Implememnt different methods to get properties

--- src/main/java/org/shiyao/framework/utils/PropsUtil.java
+++ src/main/java/org/shiyao/framework/utils/PropsUtil.java
@@ -1,5 +1,8 @@
 package org.shiyao.framework.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +12,8 @@ import java.util.Properties;
  * A util class help dealing with different properties.
  */
 public class PropsUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
+
     public static Properties loadProps(String fileName) {
         Properties props = null;
 
@@ -61,5 +66,40 @@ public class PropsUtil {
         }
     }
 
+    public static Integer getInteger(Properties props, String key) {
+        return getInteger(props, key, null);
+    }
 
+    /**
+     * Get property as Integer.
+     * @param props
+     * @param key
+     * @param defaultValue
+     * @return
+     */
+    public static Integer getInteger(Properties props, String key, Integer defaultValue) {
+        if (props.contains(key)) {
+            try {
+                Integer value = Integer.parseInt(props.getProperty(key));
+                return value;
+            } catch (Exception e) {
+                LOGGER.error("Error parsing property value: " + props.getProperty(key));
+            }
+        }
+
+        return defaultValue;
+    }
+
+    public static Boolean getBoolean(Properties props, String key) {
+        return getBoolean(props, key, false);
+    }
+
+    public static Boolean getBoolean(Properties props, String key, Boolean defaultValue) {
+        if (props.contains(key)) {
+            Boolean value = "true".equalsIgnoreCase(props.getProperty(key));
+            return value;
+        } else {
+            return defaultValue;
+        }
+    }
 }
