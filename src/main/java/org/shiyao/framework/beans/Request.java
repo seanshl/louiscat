12dc6589b0398b185546c1f2aeb90e15c76a5a7c
shiyaoeating <liushiyao2014@gmail.com>
2017-07-27 11:16:37 -0500

    Add Request

---
+++ src/main/java/org/shiyao/framework/beans/Request.java
@@ -0,0 +1,33 @@
+package org.shiyao.framework.beans;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class Request {
+    private String requestMethod;
+
+    private String requestPath;
+
+    public Request(String requestMethod, String requestPath) {
+        this.requestMethod = requestMethod;
+        this.requestPath = requestPath;
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public String getRequestPath() {
+        return requestPath;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return EqualsBuilder.reflectionEquals(this, that);
+    }
+}
