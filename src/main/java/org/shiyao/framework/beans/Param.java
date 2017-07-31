1faaf3f6d21286f4c06fac876431b0b6fbfc1677
shiyaoeating <liushiyao2014@gmail.com>
2017-07-31 11:09:14 -0500

    Change to static method and use it in Param

--- src/main/java/org/shiyao/framework/beans/Param.java
+++ src/main/java/org/shiyao/framework/beans/Param.java
@@ -1,4 +1,18 @@
 package org.shiyao.framework.beans;
 
+import org.shiyao.framework.utils.CastUtil;
+
+import java.util.Map;
+
 public class Param {
+
+    private Map<String, Object> paramMap;
+
+    public Param(Map<String, Object> paramMap) {
+        this.paramMap = paramMap;
+    }
+
+    public Long getLong(String name) {
+        return CastUtil.castLong(paramMap.get(name));
+    }
 }
--- src/main/java/org/shiyao/framework/utils/CastUtil.java
+++ src/main/java/org/shiyao/framework/utils/CastUtil.java
@@ -6,10 +6,14 @@ import org.slf4j.LoggerFactory;
 public class CastUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(CastUtil.class);
 
-    public Long castLong(String v) {
+    public static Long castLong(Object v) {
         Long res = null;
         try {
-            res = Long.valueOf(v);
+            if (v instanceof Long) {
+                res = (Long)v;
+            } else if (v instanceof String) {
+                res = Long.valueOf((String)v);
+            }
         } catch (ClassCastException e) {
             LOGGER.error("Can not cast type to Long", e.getMessage());
         }
